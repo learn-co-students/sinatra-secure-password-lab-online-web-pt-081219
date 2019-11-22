b5c9d41c08fddfcd594eb44bbb2f085b1ca09328
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-22 20:29:13 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,9 +18,8 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-    user = User.new(username: params[:username])
-    user.password_digest = params[:password]
-    user.password_confirmation = params[:password]
+
+    user = User.new(username: params[:username], password: params[:password])
     if user.save 
       redirect "/login"
     else
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20191122195645) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
